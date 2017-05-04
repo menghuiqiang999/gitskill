81ed351a5269e15b3739275d5dc12c5cc09c1a87
menghuiqiang999 <menghuiqiang@moonlightwine.com>
2017-05-04 19:17:22 +0800

    refer

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '深圳月光葡萄酒有限公司' });
+  res.render('index', { title: '深圳月光葡萄酒有限公司1' });
+    console.log("test");
 });
 
 module.exports = router;
