3d2c5ba13393e1db069a80d591e35afca44d99db
menghuiqiang999 <menghuiqiang@moonlightwine.com>
2017-05-04 18:01:23 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '深圳月光葡萄酒有限公司' });
+});
+
+module.exports = router;
